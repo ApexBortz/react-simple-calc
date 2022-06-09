e39a61a0c39b95cc116cdac091ce82befbd956ca
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-06-08 19:06:54 -0700

    Deliverable complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    num1: "",
+    num2: "",
+    result: 0
+  }
+
+  setNum = (e) => {
+    this.setState({ [e.target.name]: e.target.value});
+  }
+
+  calculate = (e) => { 
+    e.preventDefault()
+    this.setState((previousState, currentProps) => {
+      return {
+        result: parseInt(this.state.num1) + parseInt(this.state.num2)
+      }
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <div className="caclulator">
+          <h1>Add with React!</h1>
+
+          <form onSubmit={this.calculate}>
+            <input type="number" 
+                   name="num1"
+                   value={this.state.num1}
+                   onChange={this.setNum} />
+                <span>+</span>
+            <input type="number" 
+                   name="num2"
+                   value={this.state.num2}
+                   onChange={this.setNum} />
+              <button type="submit">=</button>
+              <h3>{this.state.result}</h3>
+          </form>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
